dbbdcf12fb02d69c9ac6d95a883eeab8b51e3291
Meheret Egzerab <meheretgirma22@gmail.com>
2016-12-07 19:44:31 -0700

    inital commit

---
+++ template/city/city.js
@@ -0,0 +1,32 @@
+var app = angular.module("myApp");
+app.service("CityService", ["$http", function ($http) {
+    var self = this;
+    var cities = {};
+    var nextLocation;
+    this.cityWeather = function (state) {
+        var baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + state + "&mode=json&appid=18a926c00a15bf7072316e88506a8add&units=imperial"
+        return $http.get(baseUrl)
+            .then(function (response) {
+                cities.name = response.data.name;
+                cities.weather = response.data.main.temp;
+                cities.highLow = response.data.main.temp_min;
+                cities.lowHigh = response.data.main.temp_max;
+                return cities;
+            })
+
+    };
+    
+   
+}])
+
+app.controller("cityController", ["$scope", "CityService", "SharedService",function ($scope, CityService, SharedService) {
+
+//    console.log(SharedService.savedData);
+    
+        CityService.cityWeather(SharedService.savedData)
+            .then(function (cities) {
+            console.log(cities)
+                $scope.cities= cities;
+            
+            })
+}])
---
+++ template/home/home.js
@@ -0,0 +1,48 @@
+var app = angular.module("myApp");
+
+app.service("WeatherService", ["$geolocation", "$http", function ($geolocation, $http) {
+    var self = this;
+ 
+    this.getLocation = function () {
+        return $geolocation.getCurrentPosition()
+            .then(function (position) {
+                 long = position.coords.longitude;
+                 lat = position.coords.latitude;
+                return self.weather(long, lat);
+            })
+    }
+    this.weather = function (long, lat) {
+        //     var baseUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=18a926c00a15bf7072316e88506a8add&units=metric"
+        var baseUrl = "https://api.darksky.net/forecast/37b36f0ccffe23b0d9ec4169e1fa9afc/" + lat + "," + long;
+        return $http.get(baseUrl);
+    }
+    
+//    this.suggest = function(long, lat){
+//        var baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDSS00-c1G0-ETEZQSyDuapuuxpJc89iXk";
+//        return $http.get(baseUrl)
+//    }
+    
+}])
+
+
+
+app.controller("myCtrl", ["$scope", "SharedService", "WeatherService", function ($scope, SharedService, WeatherService) {
+    
+    $scope.setNextLocation = function(state) {
+        SharedService.set(state);
+    }
+    
+    WeatherService.getLocation()
+        .then(function (response) {
+         console.log(response.data);
+        $scope.temp = response.data.currently.temperature;
+        $scope.describe = response.data.currently.icon;
+        
+    });
+    
+//    WeatherService.getLocation().
+//        .then(function(response){
+//            console.log(response)    
+//         })
+    
+}]);
